02a28112217e087b055565c0fc039b71ec6a3585
Virtual <134059573+VirtualAE@users.noreply.github.com>
2024-01-21 21:23:00 +1000

    Added assorts support, trader images, config, 3.8.0 prep
    "Side" property is now "sideExclusive" to avoid client faction type errors.

--- user/mods/Virtual's Custom Quest Loader/src/mod.ts
+++ user/mods/Virtual's Custom Quest Loader/src/mod.ts
@@ -10,6 +10,9 @@ const fs = require('fs');
 const modPath = path.normalize(path.join(__dirname, '..'));
 
 class VCQL implements IPostDBLoadMod {
+    private enableLogging
+    private enableDebugLogging
+
     public postDBLoad(container: DependencyContainer): void 
     {
         const database = container.resolve<DatabaseServer>("DatabaseServer").getTables()
@@ -17,7 +20,9 @@ class VCQL implements IPostDBLoadMod {
         const logger = container.resolve<ILogger>("WinstonLogger")
         const config = container.resolve<ConfigServer>("ConfigServer").getConfig(ConfigTypes.QUEST)
 
+        this.importConfig()
         this.importQuests(database, logger, config)
+        this.importAssorts(database, logger)
         this.importLocales(database)
         this.routeImages(imageRouter, logger)
     }
@@ -32,9 +37,16 @@ class VCQL implements IPostDBLoadMod {
         });
     }
 
+    public importConfig() {
+        let config = require(`${modPath}/database/config/config.json`)
+        this.enableLogging = config.enableLogging 
+        this.enableDebugLogging = config.enableDebugLogging
+    }
+
     public importQuests(database, logger, config) {
         let questCount = 0
 		let prunedCount = 0
+        let debugLogging = this.enableDebugLogging
         this.loadFiles(`${modPath}/database/quests/`, [".json"], function(filePath) {
             const item = require(filePath)
             if (Object.keys(item).length < 1) return 
@@ -42,25 +54,86 @@ class VCQL implements IPostDBLoadMod {
 				// Date check
 				if (item[quest].startMonth && item[quest].startMonth > 0) {
                     let currentDate = new Date()
+
                     let questStartDate = new Date(currentDate.getFullYear(), item[quest].startMonth - 1, item[quest].startDay)
                     let questEndDate = new Date(currentDate.getFullYear(), item[quest].endMonth - 1, item[quest].endDay)
+
                     if (currentDate < questStartDate || currentDate > questEndDate) {
+                        if (debugLogging) logger.success(`[VCQL-DEBUG] Removing quest ${item[quest]._id} because it is outside the date range`)
                         prunedCount++
-					    continue;
+					    continue
                     }
 				}
 				// Cleanup
-				delete item[quest].startMonth; delete item[quest].endMonth; delete item[quest].startDay; delete item[quest].endDay;
+				delete item[quest].startMonth; delete item[quest].endMonth; delete item[quest].startDay; delete item[quest].endDay
 				// Push
-                if (item[quest].side == "Usec") config.usecOnlyQuests.push(quest)
-                if (item[quest].side == "Bear") config.bearOnlyQuests.push(quest)
-                item[quest].side = "Pmc"
+                if (item[quest].sideExclusive) {
+                    if (item[quest].sideExclusive == "Usec") {
+                        config.usecOnlyQuests.push(quest)
+                        if (debugLogging) logger.success(`[VCQL-DEBUG] Adding quest ${item[quest]._id} as a Usec exclusive quest.`)
+                    }
+                    if (item[quest].sideExclusive == "Bear") {
+                        config.bearOnlyQuests.push(quest)
+                        if (debugLogging) logger.success(`[VCQL-DEBUG] Adding quest ${item[quest]._id} as a Bear exclusive quest.`)
+                    }
+                    delete item[quest].sideExclusive
+                } 
                 database.templates.quests[quest] = item[quest]
                 questCount++
             }
         })
-        logger.success(`[VCQL] Loaded ${questCount} custom quests.`)
-        logger.success(`[VCQL] ${prunedCount} custom quests were pruned due to date settings.`)
+
+        if (this.enableLogging) {
+            logger.success(`[VCQL] Loaded ${questCount} custom quests.`)
+            logger.success(`[VCQL] ${prunedCount} custom quests were pruned due to date settings.`)
+        }
+    }
+
+    public importAssorts(database, logger) {
+        let debugLogging = this.enableDebugLogging
+        this.loadFiles(`${modPath}/database/assorts/`, [".json"], function(filePath) {
+            const assorts = require(filePath)
+            if (assorts.items == undefined || assorts.traderID == undefined || assorts.barter_scheme == undefined) return
+            
+            let traderID = assorts.traderID
+            let databaseTrader = database.traders[traderID]
+
+            // If assort is for a custom trader, ensure that they have the appropriate assort tables
+            if (databaseTrader.questassort == undefined) databaseTrader.questassort = {
+                started: {},
+                success: {},
+                fail: {}
+            }
+            if (databaseTrader.assort == undefined) databaseTrader.assort = {
+                items: [],
+                barter_scheme: {},
+                loyal_level_items: {}
+            }
+            // Add barter scheme, loyalty level, quest assort and general assort, where applicable
+            for (const assort of assorts.items) {
+                if (assorts.barter_scheme[assort._id] != undefined) {
+                    databaseTrader.assort.barter_scheme[assort._id] = assorts.barter_scheme[assort._id]
+                } else {
+                    if (assort.parentId == "hideout") logger.error(`[VCQL] Parent assort ${assort._id} has no associated barter scheme.`)
+                }
+
+                if (assort.loyaltyLevel != undefined) {
+                    databaseTrader.assort.loyal_level_items[assort._id] = assort.loyaltyLevel
+                    delete assort.loyaltyLevel
+                } else {
+                    if (assort.parentId == "hideout") logger.error(`[VCQL] Parent assort ${assort._id} has no associated loyalty level`)
+                }
+
+                if (assort.unlockedOn != undefined && assort.questID != undefined) {
+                    databaseTrader.questassort[assort.unlockedOn][assort._id] = assort.questID
+                    delete assort.unlockedOn
+                    delete assort.questID
+                } 
+
+                databaseTrader.assort.items.push(assort)
+                if (debugLogging) logger.success(`[VCQL-DEBUG] Adding assort ${assort._id} to the trader ${traderID}`)
+            }
+        })
     }
 
     public importLocales(database) {
@@ -91,12 +164,26 @@ class VCQL implements IPostDBLoadMod {
     }
 
     public routeImages(imageRouter, logger) {
-        let imageCount = 0
+        let questImageCount = 0
+        let debugLogging = this.enableDebugLogging
         this.loadFiles(`${modPath}/res/quests/`, [".png", ".jpg"], function(filePath) {
             imageRouter.addRoute(`/files/quest/icon/${path.basename(filePath, path.extname(filePath))}`, filePath)
-            imageCount++
+            if (debugLogging) logger.success(`[VCQL-DEBUG] Adding quest image ${path.basename(filePath)}`)
+            questImageCount++
         })
-        logger.success(`[VCQL] Loaded ${imageCount} custom images.`)
+        logger.success(`[VCQL] Loaded ${questImageCount} custom quest images.`)
+
+        let traderImageCount = 0
+        this.loadFiles(`${modPath}/res/traders/`, [".png", ".jpg"], function(filePath) {
+            imageRouter.addRoute(`/files/trader/avatar/${path.basename(filePath, path.extname(filePath))}`, filePath)
+            if (debugLogging) logger.success(`[VCQL-DEBUG] Adding quest image ${path.basename(filePath)}`)
+            traderImageCount++
+        })
+
+        if (this.enableLogging) {
+            logger.success(`[VCQL] Loaded ${questImageCount} custom quest images.`)
+            logger.success(`[VCQL] Loaded ${traderImageCount} custom trader images.`)
+        }
     }
 }
 
