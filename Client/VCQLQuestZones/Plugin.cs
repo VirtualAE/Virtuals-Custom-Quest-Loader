29ba872b4203e6596b10bb973a83f957c21fa036
Virtual <134059573+VirtualAE@users.noreply.github.com>
2024-05-05 19:20:37 +1000

    Version fix for 381

--- Client/VCQLQuestZones/Plugin.cs
+++ Client/VCQLQuestZones/Plugin.cs
@@ -16,7 +16,7 @@ using System.Text;
 
 namespace VCQLQuestZones
 {
-    [BepInPlugin("com.virtual.vcql", "VCQL-Zones", "1.0.0")]
+    [BepInPlugin("com.virtual.vcql", "VCQL-Zones", "2.0.1")]
     [BepInProcess("EscapeFromTarkov.exe")]
     public class Plugin : BaseUnityPlugin
     {
